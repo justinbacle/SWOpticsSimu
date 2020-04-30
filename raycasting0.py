7231f0ebc655470a369d7d8bbefde667ae05ea3e
justinbacle <justin.bacle@gmail.com>
2020-04-30 02:12:59 +0200

    added few comments

--- raycasting0.py
+++ raycasting0.py
@@ -17,17 +17,20 @@ class Sensor:
 
 class Material:
     def __init__(self, color):
+        '''just color of emissivity is good for now'''
         self.color = color
 
 
 class Object:
     def __init__(self, path, material):
+        ''' an obj mesh, with an associated material would be good for now'''
         self.mesh = None
         self.material = None
 
 
 class Lens:
     def __init__(self, pos, rot):
+        ''' Lens object that contains surfaces '''
         self.pos = pos
         self.rot = rot
         self.surfaces = []
@@ -38,10 +41,13 @@ class Lens:
 
 class Surface:
     def __init__(self, radius: float, thickness: float, IOR: float, depth: float):
+        ''' Surface with all necessary items '''
         self.radius = radius
         self.thickness = thickness
         self.IOR = IOR
         self.depth = depth
+
+    # TODO compute a mesh with that ???
 
 
 class Scene:
@@ -56,6 +62,10 @@ class Scene:
 
     def show(self):
         ''' plots all the elements in the scene '''
+        pass
+
+    def display(self):
+        ''' Plot sensor image '''
         pass
 
 
@@ -73,3 +83,4 @@ o1 = Object("", m1)
 scene = Scene(s0, [l1], [o1])
 scene.cast(1e3)
 scene.show()
+scene.display()
